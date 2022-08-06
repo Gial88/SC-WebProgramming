0dd67651c19be11a21e08c63e14c3ed4087d10f6
Gial88 <muhammad.basith18@gmail.com>
2022-08-06 09:07:51 +0800

    form handling

--- pertemuan6/form.php
+++ pertemuan6/form.php
@@ -4,17 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pertemuan 5</title>
+    <title>Form Handling</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="">
+    <form action="read.php" method="">
         <label for="nama">Nama : </label>
         <input type="text" name="nama" require><br>
         <label for="nim">Nim : </label>
         <input type="number" name="nim" require maxlength="10" minlength="3"><br>
         <label for="email">Email : </label>
         <input type="text" name="email"><br>
+        <button type="submit" name="submit">Kirim</button>
     </form>
 </body>
 </html>
