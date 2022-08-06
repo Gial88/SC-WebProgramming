54481167c7647f675cdd700f1e3f945e628933fe
Gial88 <muhammad.basith18@gmail.com>
2022-08-06 08:05:06 +0800

    123

---
+++ pertemuan6/form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 5</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="">
+        <label for="nama">Nama : </label>
+        <input type="text" name="nama" require><br>
+        <label for="nim">Nim : </label>
+        <input type="number" name="nim" require maxlength="10" minlength="3"><br>
+        <label for="email">Email : </label>
+        <input type="text" name="email"><br>
+    </form>
+</body>
+</html>
